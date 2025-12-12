84fa932c7898f43037542871d15f07a5cb31f297
Harshad Bhutal <164796831+HarshadBhutal@users.noreply.github.com>
2025-12-12 20:49:44 +0530

    unwanted comments removed

--- main.py
+++ main.py
@@ -28,7 +28,7 @@ def comment_filter(title,comments,top_n):
 
 
 def videos_with_comments_from_search(query):
-    # 1. Search for videos
+
     search_request = youtube.search().list(
         part="snippet",
         q=query,
@@ -41,7 +41,6 @@ def videos_with_comments_from_search(query):
 
     all_comments = []
 
-    # 2. Loop through videos and collect comments
     for video_id in video_ids:
         try:
             comment_request = youtube.commentThreads().list(
@@ -63,11 +62,9 @@ def videos_with_comments_from_search(query):
         except Exception:
             continue
 
-    # 3. Filter by length
     filtered_by_length = long_comments(all_comments, 10)
 
-    # 4. Apply your comment_filter
-    # Use query as the title since you don't want video names
+    
     filtered_comments = comment_filter(query, filtered_by_length, 50)
 
     
